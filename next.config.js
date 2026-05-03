cca68016ec04dbda6676c56b54716c7c1866f039
Simeon Great <stargatecares@gmail.com>
2026-05-03 14:06:02 +0100

    commit all changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
+// next.config.js
 module.exports = {
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
   async rewrites() {
     return [
       {
         source: '/:slug(^[a-zA-Z0-9-]+$)',
-        destination: '/profile/:slug'
-      }
-    ]
-  }
-}
+        destination: '/profile/:slug',
+      },
+    ];
+  },
+};
